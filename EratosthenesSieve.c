710feb39bb4554b25b6898312db11c4e9bbace6e
Dylan M. Collins <dylanmcollins@gmail.com>
2024-06-19 19:31:10 +0100

    Initial commit

---
+++ EratosthenesSieve.c
@@ -0,0 +1,20 @@
+#define N 1000
+main(){
+    int i, j, a[N+1];
+    for (a[1] = 0, i = 2; i <= N; i++){
+        a[i] = 1;
+    }
+    for (i = 2; i <= N/2; i++){
+        if (a[i]){              //This line ensures that any numbers marked as nonprime are not evaluated
+            for (j = 1; i <= N/i; j++){
+                a[i*j] = 0;
+            }
+        }   
+    }
+    for ( i = 1; i <= N; i++){
+        if (a[i]){
+            printf("%4d", i);
+        }
+    }
+    printf("\n");
+}
---
+++ Euclid.c
@@ -0,0 +1,20 @@
+#include <stido.h>
+int gcd(int u, int v){
+    int t;
+    while (u > 0){
+        if (u < v) {
+            t = u; u = v; v = t;
+        }
+        u = u % v;              //The use of % instead of - increases efficiency when large and small numbers are used
+    }
+    return v;
+}
+
+int main (){
+    int x, y;
+    while (scanf("%d %d", &x, &y) != EOF){
+        if (x>0 && y>0){
+            printf("%d %d %d\n", x, y, gcd(x,y))
+        }
+    }
+}
