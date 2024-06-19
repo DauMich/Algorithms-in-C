5de786d9fa23d88040b39ef9608d8fc61656769a
Dylan M. Collins <dylanmcollins@gmail.com>
2024-06-19 19:35:32 +0100

    Update Euclid.c

--- Euclid.c
+++ Euclid.c
@@ -1,4 +1,4 @@
-#include <stido.h>
+#include <stdio.h>
 int gcd(int u, int v){
     int t;
     while (u > 0){
@@ -14,7 +14,7 @@ int main (){
     int x, y;
     while (scanf("%d %d", &x, &y) != EOF){
         if (x>0 && y>0){
-            printf("%d %d %d\n", x, y, gcd(x,y))
+            printf("%d %d %d\n", x, y, gcd(x,y));
         }
     }
 }
